648893d8ddcc6ff4ddfd2527a2bdb08ffee3cd36
Alexander Llanes <alexander.llanes@nareia.com.uy>
2019-10-03 00:01:22 -0300

    Fix UnitTest

---
+++ EconoMe/EconoMe.UnitTest/Validations/EmailValidationTest.cs
@@ -0,0 +1,54 @@
+﻿using EconoMe.Validations;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EconoMe.UnitTest.Validations
+{
+    [TestFixture]
+    public class EmailValidationTest
+    {
+        [TestCase("alex.llanes@gmail.com")]
+        [TestCase("alex@hotmail.com")]
+        public void EmailValue_ShouldBeSuccess(string email)
+        {
+            //Arrange
+            var textProperty = new ValidatableObject<string>
+            {
+                Value = email
+            };
+
+            textProperty.Validations.Add(new EmailRule()
+            {
+                ValidationMessage = "Invalid format"
+            });
+
+            //act
+            var isValidProperty = textProperty.Validate();
+
+            //Assert
+            isValidProperty.Should().BeTrue();
+        }
+
+        [TestCase("alex.llanesgmail.com")]
+        [TestCase("@hotmail.com")]
+        public void EmailValue_ShouldBeInvalid(string email)
+        {
+            //Arrange
+            var textProperty = new ValidatableObject<string>
+            {
+                Value = email
+            };
+
+            textProperty.Validations.Add(new EmailRule()
+            {
+                ValidationMessage = "Invalid format"
+            });
+
+            //act
+            var isValidProperty = textProperty.Validate();
+
+            //Assert
+            isValidProperty.Should().BeFalse();
+        }
+    }
+}
